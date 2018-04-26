887b475feba30aa3228d03f8ed96388521bc6408
Dani Damara <danny12march@gmail.com>
2018-04-26 09:52:50 +0700

    ganti express .Route() jadi Router(), sama nama method dari card controllers

--- controllers/card.js
+++ controllers/card.js
@@ -1,7 +1,7 @@
-const Card = require('../models/card')
+const Card = require('../models/Card')
 
 module.exports = {
-  ell: function (req, res) {
+  all: function (req, res) {
     Card.find(function(err, cards) {
       if (err) {
         res.status(500).send({
@@ -16,7 +16,7 @@ module.exports = {
       }
     })
   },
-  craeta: function (req, res) {
+  create: function (req, res) {
     let newCard = new Card(req.body)
     newCard.save(function (err, card) {
       if (err) {
@@ -56,7 +56,7 @@ module.exports = {
       }
     })
   },
-  delete: function (req, res) {
+  deletes: function (req, res) {
     Card.remove({ _id: req.id }, function (err, result) {
       if (err) {
         res.status(500).send({
