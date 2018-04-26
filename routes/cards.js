38db539c7e5be71d836a92a9391c0b52ce490a62
ekifakhrureza <ekifakhrureza@gmail.com>
2018-04-26 10:15:41 +0700

    checkpoint 1

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,22 @@
-const router = require('express').Route()
+const router = require('express').Router()
 const {
   all, create, update, deletes
 } = require('../controllers/card')
 
-router
-  .get('/', all)
-  .post('/', create)
-  .put('/:id', update)
-  .delete('/:id', deletes)
+router.get('/', all)
+router.post('/', create)
+router.put('/:id', update)
+router.delete('/:id', deletes)
 
 module.exports = router;
+
+// const express = require('express');
+// const router = express.Router();
+// var booksController = require('../controllers/books');
+
+// router.get('/', booksController.all)
+// router.post('/', booksController.create)
+// router.put('/:id', booksController.update)
+// router.delete('/:id', booksController.delete)
+
+// module.exports = router
