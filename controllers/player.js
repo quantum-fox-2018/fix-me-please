91a3c7b48865fe8cdc1df1d9b645ee73b24713df
barestu <fheartnet@gmail.com>
2018-04-26 10:01:02 +0700

    fix get players response

--- controllers/player.js
+++ controllers/player.js
@@ -10,7 +10,7 @@ module.exports = {
             err
           })
         } else {
-          res.send(200).status({
+          res.status(200).send({
             msg: 'success get data players',
             players
           })
@@ -26,7 +26,7 @@ module.exports = {
           err
         })
       } else {
-        res.send({
+        res.status(200).send({
           msg: 'success add data player',
           player
         })
