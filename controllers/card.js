cf14eb8b6fd3a109145d8318658e553878f79705
faizal <faizalramanda@gmail.com>
2018-04-26 10:35:44 +0700

    fix deletes function on controller, add letter r on require Router

--- controllers/card.js
+++ controllers/card.js
@@ -56,7 +56,7 @@ module.exports = {
       }
     })
   },
-  delete: function (req, res) {
+  deletes: function (req, res) {
     Card.remove({ _id: req.id }, function (err, result) {
       if (err) {
         res.status(500).send({
