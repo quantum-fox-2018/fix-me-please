fe2b1b914f7f4b6fbc8e2783ac84deffef9313f2
Wikanyaa <Mac@Macbooknyaa.localdomain>
2018-04-26 10:31:56 +0700

    fix delete cards

--- controllers/card.js
+++ controllers/card.js
@@ -58,7 +58,7 @@ module.exports = {
     })
   },
   deletes: function (req, res) {
-    Card.remove({ _id: req.id }, function (err, result) {
+    Card.remove({ _id: req.params.id }, function (err, result) {
       if (err) {
         res.status(500).send({
           msg: 'error deleting data card',
